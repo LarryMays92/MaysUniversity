401091a60cb07a0f19f284ad1db3f02c25dcc6cd
LarryMays92 <LarryMays92@ymail.com>
2022-08-24 15:56:08 -0400

    courses & students good

--- main_app/models.py
+++ main_app/models.py
@@ -6,24 +6,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    classification = models.CharField(max_length=100, blank=False) 
+
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    duration = models.CharField(max_length=100)
-    time = models.CharField(max_length=50)
-    # students = models.IntegerField()
+    students =models.ManyToManyField(Student, through = 'Enrollment')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.CharField(max_length=100)
-    classification = models.CharField(max_length=100, blank=True) 
-
-
-    def __str__(self):
-        return self.name
+class Enrollment(models.Model):
+    students = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    start = models.CharField(max_length=100, blank=False, default= '08-15-2022')
+    end = models.CharField(max_length=100,blank= False, default= '12-15-2022')
+    final_grade = models.CharField(max_length=1, blank=True, null= True)
+    instructor: models.CharField(max_length=100)
+    
+    class Meta:
+        unique_together= [['students','course']]
 
 
 # reference to linking teachers with students and courses and vice versa
